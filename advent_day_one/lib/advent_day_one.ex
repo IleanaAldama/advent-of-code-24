fae4d0c9ae5cf3b3de8fa9649632fed38d3bfba8
ileana <ileana@trykeep.com>
2024-12-10 13:11:10 -0600

    formatting

--- advent_day_one/lib/advent_day_one.ex
+++ advent_day_one/lib/advent_day_one.ex
@@ -1,8 +1,10 @@
 defmodule AdventDayOne do
   defp is_pair?(x), do: rem(x, 2) == 0
+
   def main(_args) do
     # Read the whole input in a single pass
     input = IO.read(:stdio, :eof)
+
     {list1, list2} =
       input
       |> String.split("\n")
@@ -14,9 +16,9 @@ defmodule AdventDayOne do
       |> Enum.reduce({[], []}, fn {current, index}, {list1, list2} ->
         # if index is pair, number goes to list1, otherwise to list2
         if is_pair?(index) do
-            {[current| list1], list2}
+          {[current | list1], list2}
         else
-            {list1, [current| list2]}
+          {list1, [current | list2]}
         end
       end)
 
